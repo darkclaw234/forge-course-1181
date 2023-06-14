289a2b5d0a116384c5c4dfa2f63c8c306caca378
darkclaw234 <darkshadow5386@gmail.com>
2023-06-14 16:12:34 -0600

    added deepslate cobalt ore and updated textures

--- src/main/java/net/darkclaw/mccourse/block/ModBlocks.java
+++ src/main/java/net/darkclaw/mccourse/block/ModBlocks.java
@@ -29,6 +29,10 @@ public class ModBlocks {
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(4f).requiresCorrectToolForDrops()), ModCreativeModeTab.COURSE_TAB);
 
+    public static final RegistryObject<Block> DEEPSLATE_COBALT_ORE = registerBlock("deepslate_cobalt_ore",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(4f).requiresCorrectToolForDrops()), ModCreativeModeTab.COURSE_TAB);
+
     private static <T extends Block> RegistryObject<T> registerBlock
                                     (String name, Supplier<T> block, CreativeModeTab tab){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
