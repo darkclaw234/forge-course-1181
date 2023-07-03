9bb1a591054e74cb54d4367c6831104e23b3fdaf
Nick Devine <darkshadow5386@gmail.com>
2023-07-03 01:36:18 -0600

    added luck effect when cobalt armor put on added cobalt horse armor

--- src/main/java/net/darkclaw/mccourse/item/ModItems.java
+++ src/main/java/net/darkclaw/mccourse/item/ModItems.java
@@ -1,10 +1,7 @@
 package net.darkclaw.mccourse.item;
 
 import net.darkclaw.mccourse.MCCourseMod;
-import net.darkclaw.mccourse.item.custom.CoalSliverItem;
-import net.darkclaw.mccourse.item.custom.DowsingRodItem;
-import net.darkclaw.mccourse.item.custom.LevitationSwordItem;
-import net.darkclaw.mccourse.item.custom.PaxelItem;
+import net.darkclaw.mccourse.item.custom.*;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -18,63 +15,54 @@ public class ModItems {
 
     public static final RegistryObject<Item> COBALT_INGOT = ITEMS.register("cobalt_ingot",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
-
     public static final RegistryObject<Item> COBALT_NUGGET = ITEMS.register("cobalt_nugget",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
-
     public static final RegistryObject<Item> RAW_COBALT = ITEMS.register("raw_cobalt",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
-
     public static final RegistryObject<Item> POWERED_LAPIS_LAZULI = ITEMS.register("powered_lapis_lazuli",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
-
     public static final RegistryObject<Item> DOSWING_ROD = ITEMS.register("dowsing_rod",
             () -> new DowsingRodItem(new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB).durability(64)));
 
     public static final RegistryObject<Item> COAL_SLIVER = ITEMS.register("coal_sliver",
             () -> new CoalSliverItem(new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
-
     public static final RegistryObject<Item> BLUEBERRIES = ITEMS.register("blueberries",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB).food(ModFoods.BLUEBERRIES)));
 
     public static final RegistryObject<Item> COBALT_SWORD = ITEMS.register("cobalt_sword",
             () -> new LevitationSwordItem(ModTiers.COBALT, 2, -1f,
                     new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
-
     public static final RegistryObject<Item> COBALT_PICKAXE = ITEMS.register("cobalt_pickaxe",
             () -> new PickaxeItem(ModTiers.COBALT, 0, 0f,
                     new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
-
     public static final RegistryObject<Item> COBALT_AXE = ITEMS.register("cobalt_axe",
             () -> new AxeItem(ModTiers.COBALT, 4, -3f,
                     new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
-
     public static final RegistryObject<Item> COBALT_SHOVEL = ITEMS.register("cobalt_shovel",
             () -> new ShovelItem(ModTiers.COBALT, -1, 0f,
                     new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
-
     public static final RegistryObject<Item> COBALT_HOE = ITEMS.register("cobalt_hoe",
             () -> new HoeItem(ModTiers.COBALT, -1, 0f,
                     new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
-
     public static final RegistryObject<Item> COBALT_PAXEL = ITEMS.register("cobalt_paxel",
             () -> new PaxelItem(ModTiers.COBALT, -2, 2f,
                     new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
 
     public static final RegistryObject<Item> COBALT_HELMET = ITEMS.register("cobalt_helmet",
-            () -> new ArmorItem(ModArmorMaterials.COBALT, EquipmentSlot.HEAD,
+            () -> new ModArmorItem(ModArmorMaterials.COBALT, EquipmentSlot.HEAD,
                     new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
-
     public static final RegistryObject<Item> COBALT_CHESTPLATE = ITEMS.register("cobalt_chestplate",
             () -> new ArmorItem(ModArmorMaterials.COBALT, EquipmentSlot.CHEST,
                     new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
-
     public static final RegistryObject<Item> COBALT_LEGGINGS = ITEMS.register("cobalt_leggings",
             () -> new ArmorItem(ModArmorMaterials.COBALT, EquipmentSlot.LEGS,
                     new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
-
     public static final RegistryObject<Item> COBALT_BOOTS = ITEMS.register("cobalt_boots",
             () -> new ArmorItem(ModArmorMaterials.COBALT, EquipmentSlot.FEET,
+                    new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COBALT_HORSE_ARMOR = ITEMS.register("cobalt_horse_armor",
+            () -> new HorseArmorItem(12, "cobalt",
                     new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
 
     public static void register(IEventBus eventBus){
